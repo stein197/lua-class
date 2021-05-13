b3f50b75b073a93481e70ca82a11694461eff271
STEIN197 <35802538+STEIN197@users.noreply.github.com>
2021-05-13 13:37:21 +0500

    Added "extends" keyword

--- class.lua
+++ class.lua
@@ -1,10 +1,21 @@
+Object = {
+	__lastclassdefinition = "";
+	__extends = {};
+	__implements = {};
+	instanceof = function()
+	end;
+	clone = function()
+	end;
+	new = function(...)
+	end
+}
+
 function class(name)
-	_G[name] = {}
+	_G[name] = setmetatable({}, {__index = Object})
+	Object.__lastclassdefinition = name
 	return function(descriptor)
-		descriptor.__extends = {}
-		descriptor.__implements = {}
-		-- TODO
-		function descriptor.instanceof()
+		if not descriptor then
+			return
 		end
 		function descriptor.new(...)
 			local object = {}
@@ -19,14 +30,19 @@ function class(name)
 	end
 end
 
-function extends(classlist)
-end
-
-function implements(interfacelist)
-end
-
-function new(classname)
-	return function(...)
-		return _G[classname].new(...)
+function extends(classname)
+	-- if classname:len() == 0 then error
+	_G[Object.__lastclassdefinition] = setmetatable(_G[Object.__lastclassdefinition], _G[classname])
+	return function(descriptor)
+		function descriptor.new(...)
+			local object = {}
+			if descriptor.constructor then
+				descriptor.constructor(object, ...)
+			end
+			return setmetatable(object, {__index = descriptor})
+		end
+		for k, v in pairs(descriptor) do
+			_G[Object.__lastclassdefinition][k] = v
+		end
 	end
 end
