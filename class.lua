27551fe7d939dbf0762d8305b20d194886c8e96e
STEIN197 <35802538+STEIN197@users.noreply.github.com>
2021-05-13 13:21:45 +0500

    Initial commit

---
+++ class.lua
@@ -0,0 +1,29 @@
+function class(name)
+	_G[name] = {}
+	return function(descriptor)
+		descriptor.__extends = {}
+		descriptor.__implements = {}
+		-- TODO
+		function descriptor.instanceof()
+		end
+		function descriptor.new(...)
+			local object = {}
+			if descriptor.constructor then
+				descriptor.constructor(object, ...)
+			end
+			return setmetatable(object, {__index = descriptor})
+		end
+		for k, v in pairs(descriptor) do
+			_G[name][k] = v
+		end
+	end
+end
+
+function extends(classlist)
+end
+
+function implements(interfacelist)
+end
+
+function new(classname)
+end
