2a54e4140507f6a34e1e4e53e6595936e8482285
Nail Gafarov <nil20122013@gmail.com>
2021-05-18 12:08:50 +0500

    Basic unit testing

--- TODO.md
+++ TODO.md
@@ -9,4 +9,5 @@
 - [ ] Add traits;
 - [ ] Add inner classes (via lastclassname as table instead of string);
 - [ ] Add `clone()` method;
+- [ ] Optionally add an ability to get class name;
 - [ ] Use reflection - debug and debug.getInfo, __index for _G, setfenv and etc.;
--- test/class.lua
+++ test/class.lua
@@ -1,5 +1,5 @@
-local LuaUnit = require "lib.luaunit"
-require "src.class"
+local LuaUnit = dofile "lib/luaunit.lua"
+dofile "src/class.lua"
 
 class "A" {
 
@@ -23,16 +23,67 @@ class "B" {
 	end;
 }
 
+class "Constructor" {
+
+	a = nil;
+	b = nil;
+	sum = nil;
+
+	constructor = function (self, a, b)
+		self.a = a
+		self.b = b
+		self.sum = a + b
+	end;
+}
+
 TestClass = {
 	testExistence = function ()
 		LuaUnit.assertTable(A)
 		LuaUnit.assertTable(B)
 		LuaUnit.assertNil(C)
 	end;
-	testInstantiating = function () end; -- TODO
-	testInstantiaingNotModifyingClass = function () end;
-	testInstanceof = function () end; -- TODO
-	testGetClass = function () end; -- TODO
+	testInstantiating = function ()
+		LuaUnit.assertTable(A())
+		LuaUnit.assertTable(B())
+	end;
+	testConstructor = function ()
+		local var = Constructor(2, 3)
+		LuaUnit.assertEquals(var.a, 2)
+		LuaUnit.assertEquals(var.b, 3)
+		LuaUnit.assertEquals(var.sum, 5)
+	end;
+	testInstantiaingNotModifyingClass = function ()
+		Constructor(2, 3)
+		LuaUnit.assertNil(Constructor.a)
+	end;
+	testClassesInGlobal = function ()
+		LuaUnit.assertEquals(_G['A'], A)
+		LuaUnit.assertEquals(_G['Constructor'], Constructor)
+	end;
+	testInstanceof = function ()
+		local a = A()
+		local b = B()
+		LuaUnit.assertTrue(a:instanceof "A")
+		LuaUnit.assertTrue(a:instanceof(A))
+		LuaUnit.assertTrue(b:instanceof "B")
+		LuaUnit.assertTrue(b:instanceof(B))
+		LuaUnit.assertFalse(a:instanceof "B")
+		LuaUnit.assertFalse(a:instanceof(B))
+		LuaUnit.assertFalse(B:instanceof "A")
+		LuaUnit.assertFalse(B:instanceof(A))
+		LuaUnit.assertTrue(a:instanceof "Object")
+		LuaUnit.assertTrue(a:instanceof(Object))
+		LuaUnit.assertTrue(b:instanceof "Object")
+		LuaUnit.assertTrue(b:instanceof(Object))
+		LuaUnit.assertTrue(a:instanceof(a:getClass()))
+		LuaUnit.assertTrue(b:instanceof(b:getClass()))
+	end;
+	testGetClass = function ()
+		local a = A()
+		local b = B()
+		LuaUnit.assertEquals(a:getClass(), A)
+		LuaUnit.assertEquals(b:getClass(), B)
+	end;
 }
 
 local runner = LuaUnit.LuaUnit.new()
