8c4f8523e460340cb11fb4bcee46a14994f3f37e
Nail Gafarov <nil20122013@gmail.com>
2021-05-31 10:17:58 +0500

    Completed test rewriting

--- test/TestInheritance.lua
+++ test/TestInheritance.lua
@@ -1,78 +1,85 @@
 TestInheritance = {
 
 	setupClass = function ()
-		-- class "A" {
-		-- 	field = 0;
-		-- 	constructor = function (self, f)
-		-- 		self.field = f
-		-- 	end;
-		-- 	method = function (self)
-		-- 		return self.field
-		-- 	end;
-		-- }
-		-- class "B0" extends "A" {}
-		-- class "B1" extends "A" {
-		-- 	method = function (self)
-		-- 		return "B1"
-		-- 	end
-		-- }
-		-- class "B2" extends "A" {
-		-- 	field = 2;
-		-- 	constructor = function (self, f)
-		-- 		self.field = f ^ 2
-		-- 	end
-		-- }
+		class "ExampleA" {
+			field = 0;
+			constructor = function (self, f)
+				self.field = f
+			end;
+			method = function (self)
+				return self.field
+			end;
+		}
+		class "ExampleB0" extends "ExampleA" {}
+		class "ExampleB1" extends "ExampleA" {
+			method = function (self)
+				return "B1"
+			end
+		}
+		class "ExampleB2" extends "ExampleA" {
+			field = 2;
+			constructor = function (self, f)
+				self.field = f ^ 2
+			end
+		}
 	end;
 
 	teardownClass = function ()
-		_G["A"] = nil
-		_G["B0"] = nil
-		_G["B1"] = nil
-		_G["B2"] = nil
+		_G["ExampleA"] = nil
+		_G["ExampleB0"] = nil
+		_G["ExampleB1"] = nil
+		_G["ExampleB2"] = nil
 	end;
 
 	test_extendingNotExistingClassThrowsError = function ()
 		LuaUnit.assertErrorMsgContains(
 			"Cannot find class \"NotDefined\"",
 			function ()
-				class "New" extends "NotDefined" {}
+				class "ErrorClass1" extends "NotDefined" {}
 			end
 		)
 	end;
+	
+	test_declarationErrorDeletesReference = function ()
+		pcall(function ()
+			class "ErrorClass2" extends "NotDefined" {}
+			LuaUnit.assertNil(New)
+		end)
+	end;
 
 	test_classInheritsConstructor = function ()
-		LuaUnit.assertEquals(B0(2).field, 2)
+		LuaUnit.assertEquals(ExampleB0(2).field, 2)
 	end;
 
 	test_classInheritsMethods = function ()
-		LuaUnit.assertEquals(B0():method(), 0)
+		LuaUnit.assertEquals(ExampleB0():method(), 0)
 	end;
 
 	test_classInheritsProperties = function ()
-		LuaUnit.assertEquals(B0().field, 0)
-		LuaUnit.assertEquals(B1().field, 0)
+		LuaUnit.assertEquals(ExampleB0().field, 0)
+		LuaUnit.assertEquals(ExampleB1().field, 0)
 	end;
 
-	-- test_сlassOverridesConstructor = function ()
-	-- 	LuaUnit.assertEquals(B2(4).field, 16)
-	-- end;
-
 	test_classOverridesMethods = function ()
-		LuaUnit.assertEquals(B1():method(), "B1")
+		LuaUnit.assertEquals(ExampleB1():method(), "B1")
 	end;
 
 	test_classOverridesProperties = function ()
-		LuaUnit.assertEquals(B2().field, 2)
+		LuaUnit.assertEquals(ExampleB2(3).field, 9)
 	end;
 
 	test_instanceof = function ()
-		LuaUnit.assertTrue(B0():instanceof(B0))
-		LuaUnit.assertTrue(B0():instanceof "B0")
-		LuaUnit.assertTrue(B2():instanceof "A")
-		LuaUnit.assertTrue(B3():instanceof(Object))
+		LuaUnit.assertTrue(ExampleB0():instanceof(ExampleB0))
+		LuaUnit.assertTrue(ExampleB0():instanceof "ExampleB0")
+		LuaUnit.assertTrue(ExampleB2(3):instanceof "ExampleA")
+		LuaUnit.assertTrue(ExampleB2(3):instanceof(Object))
 	end;
 
 	test_getClass = function ()
-		LuaUnit.assertEquals(B0():getClass(), B0)
+		LuaUnit.assertEquals(ExampleB0():getClass(), ExampleB0)
+	end;
+
+	test_constructorOverriding = function ()
+		LuaUnit.assertEquals(ExampleB2(4).field, 16)
 	end;
 }
