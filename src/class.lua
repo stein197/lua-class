667d9e5919703fbf9b7f95d94daf4cbbc550d49b
STEIN197 <nil20122013@gmail.com>
2021-06-06 17:45:52 +0500

    Deleted all trait functionality

--- src/class.lua
+++ src/class.lua
@@ -41,48 +41,25 @@ local function typeDecriptorHandler(descriptor)
 	local last = Type.__last
 	local meta = last.__meta
 	Type.Check.metaAbsence(meta.type, meta.name, descriptor)
-	switch (meta.type) {
-		[Type.CLASS] = function ()
-			-- TODO: Проверить что реально работает
-			if meta.traits then
-				for tName, t in pairs(meta.traits) do
-					last = setmetatable(last, {
-						__index = t
-					})
-				end
-			end
-			last = setmetatable(descriptor, {
-				__index = last;
-				__call = function (...)
-					local object = setmetatable({}, {
-						__index = _G[meta.name]
-					})
-					if descriptor.constructor then
-						descriptor.constructor(object, table.unpack(table.slice({...}, 2)))
-					end
-					object.__meta = {
-						type = Type.INSTANCE
-					}
-					return object
-				end
+	last = setmetatable(descriptor, {
+		__index = last;
+		__call = function (...)
+			local object = setmetatable({}, {
+				__index = _G[meta.name]
 			})
-			if last.__meta.parent then
-				last.__meta.parent.__meta.children[meta.name] = last
+			if descriptor.constructor then
+				descriptor.constructor(object, table.unpack(table.slice({...}, 2)))
 			end
-			_G[meta.name] = last
-		end;
-		[Type.TRAIT] = function ()
-			last = setmetatable(descriptor, {
-				__index = last
-			})
-			if last.__meta.traits then
-				for i, parent in pairs(last.__meta.traits) do
-					parent.__meta.children[last.__meta.name] = last
-				end
-			end
-			_G[meta.name] = last
-		end;
-	}
+			object.__meta = {
+				type = Type.INSTANCE
+			}
+			return object
+		end
+	})
+	if last.__meta.parent then
+		last.__meta.parent.__meta.children[meta.name] = last
+	end
+	_G[meta.name] = last
 	Type.__last = nil
 end
 
@@ -106,19 +83,13 @@ Type = {
 		end
 	end;
 
-	-- local Type.getNameFromEnum
 	getNameFromEnum = function (value)
-	-- local function Type.getNameFromEnum(value)
 		return switch (value) {
 			[Type.INSTANCE] = "instance";
 			[Type.CLASS] = "class";
 			[Type.TRAIT] = "trait";
 		}
 	end;
-
-	-- descriptorHandler = function (descriptor)
-
-	-- end;
 
 	delete = function(ref)
 		if not ref then
@@ -131,30 +102,14 @@ Type = {
 			error "Cannot delete variable. It is not a type"
 		end
 		local typeName = ref.__meta.name
-		switch (ref.__meta.type) {
-			[Type.CLASS] = function ()
-				if ref.__meta.parent then
-					ref.__meta.parent.__meta.children[typeName] = nil
-				end
-				if ref.__meta.children then
-					for childName, child in pairs(ref.__meta.children) do
-						Type.delete(child.__meta.name)
-					end
-				end
-			end;
-			[Type.TRAIT] = function ()
-				if ref.__meta.traits then
-					for parentName, parent in pairs(ref.__meta.traits) do
-						parent.__meta.children[typeName] = nil
-					end
-				end
-				if ref.__meta.children then
-					for childName, child in pairs(ref.__meta.children) do
-						Type.delete(child.__meta.name)
-					end
-				end
+		if ref.__meta.parent then
+			ref.__meta.parent.__meta.children[typeName] = nil
+		end
+		if ref.__meta.children then
+			for childName, child in pairs(ref.__meta.children) do
+				Type.delete(child.__meta.name)
 			end
-		}
+		end
 		_G[typeName] = nil
 	end;
 
@@ -252,98 +207,26 @@ function class(name)
 	return typeDecriptorHandler
 end
 
--- Multiple inheritance instead of traiting?
-function trait(name)
-	Type.Check.name(Type.TRAIT, name)
-	Type.Check.absence(Type.TRAIT, name)
-	local ref = {
-		__meta = {
-			name = name,
-			type = Type.TRAIT
-		}
-	}
-	_G[name] = ref
-	Type.__last = ref
-	return typeDecriptorHandler
-end
-
--- TODO: Merge switch branches, use an array of single element for class
 function extends(...)
 	local lastType = Type.__last
 	local typeList = {...}
-	switch (lastType.__meta.type) {
-		[Type.CLASS] = function ()
-			Type.Check.extending(Type.CLASS, lastType.__meta.name, typeList)
-			local className = typeList[1]
-			local parent = Type.find(className)
-			if not parent then
-				Type.deleteLast()
-				error("Cannot find class \""..className.."\"")
-			end
-			if parent.__meta.type ~= Type.CLASS then
-				Type.deleteLast()
-				error("Class cannot extend "..Type.getNameFromEnum(parent.__meta.type).." \""..className.."\"")
-			end
-			lastType.__meta.parent = parent
-			_G[lastType.__meta.name] = setmetatable(lastType, {
-				__index = parent
-			})
-			if not parent.__meta.children then
-				parent.__meta.children = {}
-			end
-		end;
-		[Type.TRAIT] = function ()
-			lastType.__meta.traits = {}
-			for i, t in pairs(typeList) do
-				local parent = Type.find(t)
-				if not parent then
-					Type.deleteLast()
-					error("Cannot find trait \""..t.."\"")
-				end
-				if parent.__meta.type ~= Type.TRAIT then
-					Type.deleteLast()
-					error("Trait cannot extend "..Type.getNameFromEnum(parent.__meta.name).." \""..t.."\"")
-				end
-				-- TODO: Если возникнет ошибка, то ссылки в parent не удалятся
-				if not parent.__meta.children then
-					parent.__meta.children = {}
-				end
-				lastType.__meta.traits[parent.__meta.name] = parent
-			end
-			setmetatable(Type.__last, {
-				__index = function (self, key)
-					for tName, t in pairs(self.__meta.traits) do
-						if t[key] then
-							return t[key]
-						end
-					end
-				end;
-			})
-		end;
-	}
-	return typeDecriptorHandler
-end
-
-function uses(...)
-	local traitList = {...}
-	local lastType = Type.__last
-	local lastTypeMeta = lastType.__meta
-	if lastTypeMeta.type ~= Type.CLASS then
+	Type.Check.extending(Type.CLASS, lastType.__meta.name, typeList)
+	local className = typeList[1]
+	local parent = Type.find(className)
+	if not parent then
 		Type.deleteLast()
-		error("\""..lastTypeMeta.name.."\" is not a class. Only classes can use traits")
+		error("Cannot find class \""..className.."\"")
 	end
-	lastTypeMeta.traits = {}
-	for i, ref in pairs(traitList) do
-		local foundRef = Type.find(ref)
-		if not foundRef then
-			Type.deleteLast()
-			error("Cannot find trait at index "..i)
-		end
-		if foundRef.__meta.type ~= Type.TRAIT then
-			Type.deleteLast()
-			error("\""..foundRef.__meta.name.."\" is not a trait")
-		end
-		lastTypeMeta.traits[foundRef.__meta.name] = foundRef
+	if parent.__meta.type ~= Type.CLASS then
+		Type.deleteLast()
+		error("Class cannot extend "..Type.getNameFromEnum(parent.__meta.type).." \""..className.."\"")
+	end
+	lastType.__meta.parent = parent
+	_G[lastType.__meta.name] = setmetatable(lastType, {
+		__index = parent
+	})
+	if not parent.__meta.children then
+		parent.__meta.children = {}
 	end
 	return typeDecriptorHandler
 end
@@ -415,50 +298,6 @@ class "Class" extends 'TypeBase' {
 
 	getParent = function (self)
 		return self:getMeta("parent")
-	end;
-
-	getChildren = function (self)
-		return self:getMeta("children")
-	end;
-
-	getTraits = function (self)
-		return self:getMeta("traits")
-	end;
-
-	uses = function (self, ...)
-		local tList = {...}
-		local classTraitList = self:getTraits()
-		for i, t in pairs(tList) do
-			local tRef = Type.find(t)
-			local matches = false
-			for tName, classTrait in pairs(classTraitList) do 
-				if tRef == classTrait then
-					matches = true
-					break
-				end
-			end
-			if not matches then
-				return false
-			end
-		end
-		return true
-	end
-}
-
-class "Trait" extends 'TypeBase' {
-
-	constructor = function (self, ref)
-		if not ref then
-			error "Trait reference cannot be nil"
-		end
-		self.ref = Type.find(ref)
-		if not self.ref then
-			error("Cannot find trait \""..ref.."\"")
-		end
-	end;
-
-	getTraits = function (self)
-		return self:getMeta("traits")
 	end;
 
 	getChildren = function (self)
