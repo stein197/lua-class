51fff64e395630377a7365f10f68232515b36ef9
Nail Gafarov <nil20122013@gmail.com>
2021-06-08 13:39:53 +0500

    Added test stubs

--- test/TestMultipleInheritance.lua
+++ test/TestMultipleInheritance.lua
@@ -1,3 +1,9 @@
 TestMultipleInheritance = {
-
+	["test: Class can derive from multiple classes"] = function () error "Not implemented" end; -- TODO
+	["test: Class cannot derive already derived class"] = function () error "Not implemented" end; -- TODO
+	["test: Declaration error is thrown when methods collide in base classes"] = function () error "Not implemented" end; -- TODO
+	["test: Class inherits all methods from base classes"] = function () error "Not implemented" end; -- TODO
+	["test: Class can override methods from base classes"] = function () error "Not implemented" end; -- TODO
+	["test: Classes won't be created after declaration errors"] = function () error "Not implemented" end; -- TODO
+	["test: instanceof()"] = function () error "Not implemented" end; -- TODO
 }
