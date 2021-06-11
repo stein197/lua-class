f9e4f5cebbc9a9d91a679571eff96e3343223b46
Nail Gafarov <nil20122013@gmail.com>
2021-06-11 17:57:28 +0500

    Added test stubs

--- test/TestClone.lua
+++ test/TestClone.lua
@@ -1,1 +1,21 @@
-TestClone = {}
+TestClone = {
+
+	setupClass = function ()
+		class 'A' {
+			a = 1;
+			b = 2;
+			m = function (self)
+				return self.a..self.b
+			end;
+		}
+	end;
+
+	teardownClass = function ()
+		Type.delete(A)
+	end;
+
+	["test: clone() returns cloned object"] = function () error "Not implemented" end; -- TODO
+	["test: clone() preserves derived methods"] = function () error "Not implemented" end; -- TODO
+	["test: clone() processes deep clone"] = function () error "Not implemented" end; -- TODO
+	["test: clone() clones instance fields"] = function () error "Not implemented" end; -- TODO
+}
