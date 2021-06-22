f7d66c2319c394cbf42259078a62f33db2700cd2
Nail Gafarov <nil20122013@gmail.com>
2021-06-22 14:18:30 +0500

    Added base switch test

--- test/TestImport.lua
+++ test/TestImport.lua
@@ -4,17 +4,21 @@ TestImport = {
 		os.execute("mkdir src\\system")
 		os.execute("mkdir src\\graphics")
 		os.execute("mkdir src\\all")
+		os.execute("mkdir scripts")
+		os.execute("mkdir scripts\\system")
 		os.execute("echo import \"system.Directory\"; namespace \"system\" {class \"File\" {getDir = function () return system.Directory() end}} > src\\system\\File.lua")
 		os.execute("echo namespace \"system\" {class \"Directory\" {}} > src\\system\\Directory.lua")
 		os.execute("echo namespace \"graphics\" {class \"Point\" {}} > src\\graphics\\Point.lua")
 		os.execute("echo namespace \"all\" {class \"A\" {}} > src\\all\\A.lua")
 		os.execute("echo namespace \"all\" {class \"B\" {}} > src\\all\\B.lua")
+		os.execute("echo namespace \"system\" {class \"C\" {}} > scripts\\system\\C.lua")
 	end;
 
 	teardownClass = function ()
-		os.execute("rmdir /s /q src\\system src\\graphics src\\all")
+		os.execute("rmdir /s /q src\\system src\\graphics src\\all scripts")
 		Type.delete("system.File")
 		Type.delete("system.Directory")
+		Type.delete("system.C")
 		Type.delete("graphics.Point")
 		Type.delete("all.A")
 		Type.delete("all.B")
@@ -34,5 +38,10 @@ TestImport = {
 		all.B()
 	end;
 
-	["test: Switching base path works"] = function () error "Not implemented" end; -- TODO
+	["test: Switching base path works"] = function ()
+		Type.setBasePath("scripts")
+		import "system.C"
+		system.C()
+		Type.setBasePath("src")
+	end;
 }
