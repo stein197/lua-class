8cefe08000a9c6f79de87e82e72a23a17d84f78f
STEIN197 <nil20122013@gmail.com>
2021-06-22 19:50:23 +0500

    Added unix command for import testing

--- test/TestImport.lua
+++ test/TestImport.lua
@@ -1,21 +1,33 @@
+local ds = package.config:sub(1, 1)
+
 TestImport = {
 
 	setupClass = function ()
-		os.execute("mkdir src\\system")
-		os.execute("mkdir src\\graphics")
-		os.execute("mkdir src\\all")
+		local q = "\""
+		local esc = ""
+		if ds == "/" then
+			q = "\\"..q
+			esc = "\""
+		end		
+		os.execute("mkdir src"..ds.."system")
+		os.execute("mkdir src"..ds.."graphics")
+		os.execute("mkdir src"..ds.."all")
 		os.execute("mkdir scripts")
-		os.execute("mkdir scripts\\system")
-		os.execute("echo import \"system.Directory\"; namespace \"system\" {class \"File\" {getDir = function () return system.Directory() end}} > src\\system\\File.lua")
-		os.execute("echo namespace \"system\" {class \"Directory\" {}} > src\\system\\Directory.lua")
-		os.execute("echo namespace \"graphics\" {class \"Point\" {}} > src\\graphics\\Point.lua")
-		os.execute("echo namespace \"all\" {class \"A\" {}} > src\\all\\A.lua")
-		os.execute("echo namespace \"all\" {class \"B\" {}} > src\\all\\B.lua")
-		os.execute("echo namespace \"system\" {class \"C\" {}} > scripts\\system\\C.lua")
+		os.execute("mkdir scripts"..ds.."system")
+		os.execute("echo "..esc.."import "..q.."system.Directory"..q.."; namespace "..q.."system"..q.." {class "..q.."File"..q.." {getDir = function () return system.Directory() end}}"..esc.." > src"..ds.."system"..ds.."File.lua")
+		os.execute("echo "..esc.."namespace "..q.."system"..q.." {class "..q.."Directory"..q.." {}}"..esc.." > src"..ds.."system"..ds.."Directory.lua")
+		os.execute("echo "..esc.."namespace "..q.."graphics"..q.." {class "..q.."Point"..q.." {}}"..esc.." > src"..ds.."graphics"..ds.."Point.lua")
+		os.execute("echo "..esc.."namespace "..q.."all"..q.." {class "..q.."A"..q.." {}}"..esc.." > src"..ds.."all"..ds.."A.lua")
+		os.execute("echo "..esc.."namespace "..q.."all"..q.." {class "..q.."B"..q.." {}}"..esc.." > src"..ds.."all"..ds.."B.lua")
+		os.execute("echo "..esc.."namespace "..q.."system"..q.." {class "..q.."C"..q.." {}}"..esc.." > scripts"..ds.."system"..ds.."C.lua")
 	end;
 
 	teardownClass = function ()
-		os.execute("rmdir /s /q src\\system src\\graphics src\\all scripts")
+		if ds == "/" then
+			os.execute("rm -r src/system src/graphics src/all scripts")
+		else
+			os.execute("rmdir /s /q src\\system src\\graphics src\\all scripts")
+		end
 		Type.delete("system.File")
 		Type.delete("system.Directory")
 		Type.delete("system.C")
